ed1925b5a34a7473e585e28ca0d13cf303af353c
Martin Klotzman <martinklotzman@Martins-MacBook-Air.local>
2017-08-04 13:22:06 -0700

    spacing

--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -30,10 +30,11 @@
                 { title: 'Can you hear me now?', duration: '3:14' },
                 { title: 'Wrong phone number', duration: '2:15' }
             ]
+        };
 
         Fixtures.getAlbum = function() {
             return albumPicasso;
-        };
+            };
 
         Fixtures.getCollection = function(numberOfAlbums) {
             var albumCollection = [];
