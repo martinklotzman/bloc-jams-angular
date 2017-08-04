fc58dcc2ce40ef7f03c3c9133e7787cd71271673
Martin Klotzman <martinklotzman@Martins-MacBook-Air.local>
2017-08-04 11:54:34 -0700

    fixtures factory

---
+++ app/scripts/services/Fixtures.js
@@ -0,0 +1,10 @@
+(function() {
+    function Fixtures() {
+        var Fixtures = {};
+        return Fixtures;
+    }
+
+    angular
+        .module('blocJams')
+        .factory('Fixtures', Fixtures);
+})();
