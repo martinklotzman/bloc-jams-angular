465f085704e8185f80b27da1fc48ce4e6e49f293
Martin Klotzman <martinklotzman@Martins-MacBook-Air.local>
2017-08-05 20:26:51 -0700

    chapter 8 assignment

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -40,10 +40,19 @@
          * @desc Plays the currentBuzzObject audio file
          * @param {Object} song
          */
-
          var playSong = function(song) {
             currentBuzzObject.play()
             song.playing = true;
+         };
+
+         /**
+         * @function stopSong
+         * @desc Stops the currentBuzzObject audio file
+         * @param {Object} song
+         */
+         var stopSong = function(song) {
+            currentBuzzObject.stop();
+            song.playing = null;
          };
 
          /**
@@ -101,8 +110,25 @@
               currentSongIndex--;
 
               if (currentSongIndex < 0) {
-                  currentBuzzObject.stop();
-                  SongPlayer.currentSong.playing = null;
+                  stopSong(SongPlayer.currentSong);
+              } else {
+                  var song = currentAlbum.songs[currentSongIndex];
+                  setSong(song);
+                  playSong(song);
+              }
+          };
+
+          /**
+          * @function SongPlayer.next
+          * @desc Plays the next indexed song
+          * @param {Object}
+          */
+          SongPlayer.next = function() {
+              var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+              currentSongIndex++;
+
+              if (currentSongIndex > currentAlbum.songs.length -1) {
+                  stopSong(SongPlayer.currentSong);
               } else {
                   var song = currentAlbum.songs[currentSongIndex];
                   setSong(song);
